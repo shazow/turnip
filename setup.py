396389acabd941ba2582e78b1f2a52db95a3ed7e
Andrey Petrov <andrey.petrov@shazow.net>
2011-01-27 18:47:21 -0800

    Import

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='turnip',
+      version=version,
+      description="Task scheduler.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Andrey Petrov',
+      author_email='andrey.petrov@shazow.net',
+      url='http://github.com/shazow/turnip',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
