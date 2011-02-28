cdc8034b77bab5955eba3b03d24f0e5d10f2a328
Andrey Petrov <andrey.petrov@shazow.net>
2011-02-28 19:17:59 +0100

    formatting.

--- turnip/worker.py
+++ turnip/worker.py
@@ -95,7 +95,7 @@ class Worker(object):
         except Exception, e:
             Session.rollback()
 
-            self.log.error("Unexpected task error, will retry later:  \n{0}\n  {1}".format(repr(e)))
+            self.log.error("Unexpected task error, will retry later: {0}".format(repr(e)))
             traceback.print_exc()
             new = t.retry(self, delay=60*24)
 
